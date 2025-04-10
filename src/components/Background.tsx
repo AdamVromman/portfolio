ec5382924e220ac22080443f7bee778207f19ca3
AdamVromman <adam.vromman@gmail.com>
2025-04-10 23:21:18 +0200

    redo building part a lil

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -10,7 +10,7 @@ const Background = () => {
   const backgroundSVGRef = useRef<SVGSVGElement>(null);
   const timelineRef = useRef<GSAPTimeline | null>(null);
 
-  useGSAP(
+  const { contextSafe } = useGSAP(
     () => {
       timelineRef.current = gsap.timeline();
     },
@@ -22,7 +22,7 @@ const Background = () => {
   const calculateTileWidth = () => {
     if (backgroundSVGRef.current) {
       const localTileWidth =
-        backgroundSVGRef.current.clientWidth / (NR_OF_COLUMNS + 1);
+        backgroundSVGRef.current.clientWidth / NR_OF_COLUMNS;
       setTileWidth(localTileWidth);
       console.log("Tile Width: ", localTileWidth);
       return localTileWidth;
@@ -30,88 +30,139 @@ const Background = () => {
     return 0;
   };
 
-  const drawBackground = () => {
+  const drawLines = contextSafe((animated: boolean = false) => {
     if (backgroundSVGRef.current) {
       backgroundSVGRef.current.textContent = "";
-
-      drawLines(LineDirection.VERTICAL, LineType.MAIN);
-      drawLines(LineDirection.HORIZONTAL, LineType.MAIN);
-      drawLines(LineDirection.VERTICAL, LineType.SUB);
-      drawLines(LineDirection.HORIZONTAL, LineType.SUB);
-    }
-  };
-
-  const drawLines = (lineDirection: LineDirection, lineType: LineType) => {
-    if (backgroundSVGRef.current) {
-      const heightOrWidth =
-        lineDirection === LineDirection.VERTICAL ? "width" : "height";
       const localTileWidth = calculateTileWidth();
-      const width = backgroundSVGRef.current.clientWidth;
-      const height = backgroundSVGRef.current.clientHeight;
+      const NR_OF_ROWS = Math.floor((window.innerHeight - 30) / localTileWidth);
+      backgroundSVGRef.current.setAttribute(
+        "height",
+        NR_OF_ROWS * localTileWidth + "px"
+      );
       const svgNS = "http://www.w3.org/2000/svg";
 
-      const mainLine = document.createElementNS(svgNS, "rect");
-      mainLine.classList.add("background_svg_line");
-
-      if (lineDirection === LineDirection.VERTICAL) {
-        mainLine.classList.add("line_vertical");
-        mainLine.setAttribute("y", "0");
-        mainLine.setAttribute("height", "0");
-      } else {
-        mainLine.classList.add("line_horizontal");
-        mainLine.setAttribute("x", "0");
-        mainLine.setAttribute("width", "0");
-      }
-
-      if (lineType === LineType.MAIN) {
-        mainLine.classList.add("line_main");
-        mainLine.setAttribute(heightOrWidth, "2px");
-      } else if (lineType === LineType.SUB) {
-        mainLine.classList.add("line_sub");
-        mainLine.setAttribute(heightOrWidth, "1px");
-      }
-
-      for (
-        let i = 0;
-        i < NR_OF_COLUMNS + (lineType === LineType.MAIN ? 0 : 1);
-        i++
-      ) {
-        const line = mainLine.cloneNode() as SVGElement;
+      for (let i = 0; i < NR_OF_COLUMNS + 1; i++) {
+        const line = document.createElementNS(svgNS, "rect");
+        line.classList.add("background_svg_line");
+        line.classList.add("line_vertical");
+        line.classList.add("line_main");
+        line.setAttribute("height", "0");
+        line.setAttribute("width", "2px");
         line.setAttribute(
-          lineDirection === LineDirection.VERTICAL ? "x" : "y",
+          "x",
+          `${localTileWidth * i - (2 / (NR_OF_COLUMNS + 1)) * i}px`
+        );
+        line.setAttribute("y", "0");
+        backgroundSVGRef.current.appendChild(line);
+        if (animated) {
+          timelineRef.current?.to(
+            line,
+            {
+              duration: 1,
+              attr: { height: "100%" },
+              ease: "power4.out",
+            },
+            "<0.01"
+          );
+        } else {
+          line.setAttribute("height", "100%");
+        }
+      }
+
+      for (let i = 0; i < NR_OF_COLUMNS; i++) {
+        const line = document.createElementNS(svgNS, "rect");
+        line.classList.add("background_svg_line");
+        line.classList.add("line_vertical");
+        line.classList.add("line_sub");
+        line.setAttribute("height", "0");
+        line.setAttribute("width", "0.5px");
+        line.setAttribute(
+          "x",
           `${
-            localTileWidth / (lineType === LineType.MAIN ? 1 : 2) +
-            localTileWidth * i
+            localTileWidth / 2 + localTileWidth * i - (1 / NR_OF_COLUMNS) * i
           }px`
         );
-        line.setAttribute(
-          lineDirection === LineDirection.VERTICAL ? "y" : "x",
-          "0"
-        );
+        line.setAttribute("y", "0");
         backgroundSVGRef.current.appendChild(line);
-        gsap.to(line, {
-          duration: 1,
-          attr:
-            lineDirection === LineDirection.VERTICAL
-              ? { height: "100%" }
-              : { width: "100%" },
-          delay: 0.05 * i,
-          ease: "power4.out",
-        });
+        if (animated) {
+          timelineRef.current?.to(
+            line,
+            {
+              duration: 1,
+              attr: { height: "100%" },
+              ease: "power4.out",
+            },
+            "<0.01"
+          );
+        } else line.setAttribute("height", "100%");
+      }
+
+      for (let i = 0; i < NR_OF_ROWS + 1; i++) {
+        const line = document.createElementNS(svgNS, "rect");
+        line.classList.add("background_svg_line");
+        line.classList.add("line_horizontal");
+        line.classList.add("line_main");
+        line.setAttribute("width", "0");
+        line.setAttribute("height", "2px");
+        line.setAttribute(
+          "y",
+          `${localTileWidth * i - (2 / (NR_OF_ROWS + 1)) * i}px`
+        );
+        line.setAttribute("x", "0");
+        backgroundSVGRef.current.appendChild(line);
+        if (animated) {
+          timelineRef.current?.to(
+            line,
+            {
+              duration: 1,
+              attr: { width: "100%" },
+              ease: "power4.out",
+            },
+            "<0.01"
+          );
+        } else line.setAttribute("width", "100%");
+      }
+
+      for (let i = 0; i < NR_OF_ROWS; i++) {
+        const line = document.createElementNS(svgNS, "rect");
+        line.classList.add("background_svg_line");
+        line.classList.add("line_horizontal");
+        line.classList.add("line_sub");
+        line.setAttribute("width", "0");
+        line.setAttribute("height", "0.5px");
+        line.setAttribute(
+          "y",
+          `${localTileWidth / 2 + localTileWidth * i - (1 / NR_OF_ROWS) * i}px`
+        );
+        line.setAttribute("x", "0");
+        backgroundSVGRef.current.appendChild(line);
+        if (animated) {
+          timelineRef.current?.to(
+            line,
+            {
+              duration: 1,
+              attr: { width: "100%" },
+              ease: "power4.out",
+            },
+            "<0.01"
+          );
+        } else line.setAttribute("width", "100%");
       }
     }
+  });
+
+  const onResize = () => {
+    calculateTileWidth();
+    drawLines();
   };
 
   useEffect(() => {
     calculateTileWidth();
-    drawBackground();
-    window.addEventListener("resize", () => {
-      calculateTileWidth();
-      drawBackground();
-    });
+    drawLines(true);
+    window.addEventListener("resize", onResize);
 
     return () => {
-      window.removeEventListener("resize", calculateTileWidth);
+      window.removeEventListener("resize", onResize);
     };
   }, []);
 
