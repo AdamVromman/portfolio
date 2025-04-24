4d3be32922b10ecd479419a3cf0361494f8c83d9
AdamVromman <adam.vromman@gmail.com>
2025-04-24 21:42:16 +0200

    add new icons

--- src/assets/Constants.ts
+++ src/assets/Constants.ts
@@ -8,6 +8,18 @@ export const NR_OF_COLUMNS_1280 = 30;
 
 export const projects: Project[] = [
   {
+    name: "Qausal",
+    slug: "qausal",
+    color: "de2217",
+    year: 2025,
+  },
+  {
+    name: "Transfo Intiem",
+    slug: "transfo-intiem",
+    color: "de2217",
+    year: 2024,
+  },
+  {
     name: "Train World",
     slug: "train-world",
     color: "de2217",
