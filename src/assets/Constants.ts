4066135fd700fc4966bf133409d3a94e0c7790e2
AdamVromman <adam.vromman@gmail.com>
2025-06-25 17:44:08 +0200

    upload train world + transfo intiem files

--- src/assets/Constants.ts
+++ src/assets/Constants.ts
@@ -10,8 +10,8 @@ export const ROWS_PER_PROJECT_PER_SCREENSIZE = new Map<
   string,
   { xs: number; sm: number; md: number; lg: number; xl: number }
 >([
-  ["club-sofa", { xs: 115, sm: 138, md: 121, lg: 109, xl: 107 }],
-  ["train-world", { xs: 5, sm: 5, md: 5, lg: 5, xl: 5 }],
+  ["club-sofa", { xs: 113, sm: 131, md: 121, lg: 113, xl: 111 }],
+  ["train-world", { xs: 115, sm: 138, md: 121, lg: 109, xl: 107 }],
   ["transfo-intiem", { xs: 5, sm: 5, md: 5, lg: 5, xl: 5 }],
   ["all-eyes-on-gaza", { xs: 5, sm: 5, md: 5, lg: 5, xl: 5 }],
   ["qausal", { xs: 5, sm: 5, md: 5, lg: 5, xl: 5 }],
@@ -30,12 +30,12 @@ export const projects: Project[] = [
     color: "de2217",
     year: 2023,
   },
-  // {
-  //   name: "Transfo Intiem",
-  //   slug: "transfo-intiem",
-  //   color: "B87AFF",
-  //   year: 2024,
-  // },
+  {
+    name: "Transfo Intiem",
+    slug: "transfo-intiem",
+    color: "B87AFF",
+    year: 2024,
+  },
   // {
   //   name: "All Eyes on Gaza",
   //   slug: "all-eyes-on-gaza",
