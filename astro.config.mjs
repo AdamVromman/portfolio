19732e48eb5ec4744959149e7a4995d230626710
AdamVromman <adam.vromman@gmail.com>
2025-08-01 15:54:03 +0200

    add matomo package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import matomo from "astro-matomo";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [
+    matomo({
+      enabled: true,
+      host: "https://adamvromman.net/analytics/",
+      siteId: 1,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+      viewTransition: true,
+    }),
+  ],
 });
