4d097f21c271aa7c7df45aefa50e6fd14277830a
AdamVromman <adam.vromman@gmail.com>
2025-08-05 14:34:45 +0200

    add more dutch

--- src/assets/ServerFunctions.ts
+++ src/assets/ServerFunctions.ts
@@ -3,3 +3,22 @@ import { Language } from "./Constants";
 export const getLanguage = (astroInput: any): Language => {
   return (astroInput as Language) || Language.EN;
 };
+
+export const slugToLanguageKey = (
+  slug: string
+): "clubSofa" | "trainWorld" | "transfoIntiem" | "qausal" | "allEyesOnGaza" => {
+  switch (slug) {
+    case "club-sofa":
+      return "clubSofa";
+    case "train-world":
+      return "trainWorld";
+    case "transfo-intiem":
+      return "transfoIntiem";
+    case "qausal":
+      return "qausal";
+    case "all-eyes-on-gaza":
+      return "allEyesOnGaza";
+    default:
+      return "clubSofa";
+  }
+};
